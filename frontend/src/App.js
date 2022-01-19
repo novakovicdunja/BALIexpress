ab6d30ee8e1451830c99c58e3944c7d12e9a9087
novakovicdunja <62376406+novakovicdunja@users.noreply.github.com>
2022-01-19 22:52:29 +0100

    Added HomeScreen.js  and ProductScreen.js. Created routes in App.js towards these components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
-import data from './data';
-import Product from './components/Product';
+import {BrowserRouter, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
         <header className="row">
             <div>
@@ -13,19 +16,14 @@ function App() {
             </div>
         </header>
         <main>
-            <div className="row center">
-              {
-                data.products.map(product =>  (
-                  <Product key={product._id} product={product}/>
-                ))
-              }
-              
-            </div>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">
             Copyright by Dunja
         </footer>
     </div>
+    </BrowserRouter>
   );
 }
 
