39afc5d913bb1a785709dd3d97c3e116f37d2021
novakovicdunja <62376406+novakovicdunja@users.noreply.github.com>
2022-01-30 21:10:03 +0100

    Added payment method screen. Added cart action: savepaymentmethod. Shipping address info is removed before signout.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import SignInScreen from './screens/SignInScreen';
 import { signout } from './actions/userActions';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -59,6 +60,7 @@ function App() {
           <Route path="/signin" component={SignInScreen}></Route>
           <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/shipping" component={ShippingAddressScreen}></Route>
+          <Route path="/payment" component={PaymentMethodScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">ITEH - Dunja</footer>
